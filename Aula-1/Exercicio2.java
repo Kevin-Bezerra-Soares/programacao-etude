177ae7c692130af19a17a4a346d89e13bb8d46bb
Kevin-pyth <62070604+Kevin-pyth@users.noreply.github.com>
2025-02-15 13:43:15 +0000

    main

---
+++ Aula-1/Exercicio2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main (String[] args) {
+        int num1, num2, num3, soma;
+        java.util.Scanner entrada = new Scanner(System.in);
+
+        System.out.print (" Digite um número inteiro: ");
+        num1 = entrada.nextInt();
+
+        System.out.print (" Digite outro número inteiro: ");
+        num2 = entrada.nextInt();
+
+        System.out.print (" Digite o último número inteiro: ");
+        num3 = entrada.nextInt();
+
+        soma = ((num1 + num2 + num3) / 3);
+        System.out.println (" A média aritmética é: "+ soma);
+
+    }
+    
+}
