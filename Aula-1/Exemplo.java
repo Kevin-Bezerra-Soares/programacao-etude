6605a4f54d027a931a837b4436a38e4f784bdbe2
Kevin-pyth <62070604+Kevin-pyth@users.noreply.github.com>
2025-02-15 13:05:15 +0000

    main

--- Aula-1/Exemplo.java
+++ Aula-1/Exemplo.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class Exemplo {
     public static void main (String [] args) {
         int numero1, numero2, soma;
-        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print (" Digite um número: ");
+        numero1 = entrada.nextInt();
+
+        System.out.print (" Didgite outro número: ");
+        numero2 = entrada.nextInt();
+
+        soma = numero1 + numero2;
+        System.out.println (" Total da soma: "+ soma);
     }
 }
