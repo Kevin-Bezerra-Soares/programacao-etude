852d89af9488306d3bdefc583e303cf16b54ad09
Kevin-pyth <62070604+Kevin-pyth@users.noreply.github.com>
2025-02-26 21:12:37 +0000

    main

--- Linguagem de Programação/Exercicio1_2.java
+++ Linguagem de Programação/Exercicio1_2.java
@@ -1,5 +1,25 @@
 import java.util.Scanner;
 
 public class Exercicio1_2 {
+    public static void main(String[] args) {
+        int anos, meses, dias, resp;
+        
+        Scanner val = new Scanner(System.in);
+
+        System.out.print (" Informe quantos anos você tem: ");
+        anos = val.nextInt();
+
+        System.out.print (" Informe quantos meses além da idade: ");
+        meses = val.nextInt();
+
+        System.out.print (" Informa quantos dias além dos anos e meses.");
+        dias = val.nextInt();
+
+        resp = ((anos * 365) + (meses * 30) + dias);
+        
+        System.out.println(" Sua idade em dias é: " + resp);
+
+
+    }
 
 }
