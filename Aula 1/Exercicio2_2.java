120ce3e0b877ce31db7d01f40053a40fcabbde18
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-02-28 20:49:00 +0000

    main

--- Aula 1/Exercicio2_2.java
+++ Aula 1/Exercicio2_2.java
@@ -3,6 +3,21 @@
 import java.util.Scanner;
 
 public class Exercicio2_2 {
-    
+    public static void main(String[]argis){
+        float area, largura, profundidade;
+        Scanner ent = new Scanner(System.in);
+
+        System.out.print(" Informe a largura do terreno: ");
+        largura = ent.nextFloat();
+
+        System.out.print(" Informe a profundidade do terreno: ");
+        profundidade = ent.nextFloat();
+
+        area = (largura*profundidade);
+
+        System.out.println(" A área do terreno é: " + area);
+
+
+    }
     
 }
