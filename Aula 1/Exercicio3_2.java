649ea5a2cac09a67ffaecae85fe5371917558abf
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-02-28 21:04:06 +0000

    main

---
+++ Aula 1/Exercicio3_2.java
@@ -0,0 +1,17 @@
+// O programa lê o custo de fábrica de um automóvel e retorna a soma da porcentagem do distribuidor e impostos;
+// Aula 2;
+
+import java.util.Scanner;
+
+public class Exercicio3_2 {
+    public static void main (String[] args){
+        float vfabrica, vdist, vimposto;
+        Scanner entr = new Scanner(System.in);
+
+        System.out.print(" Informe o custo de fábrica do veículo: ");
+        vfabrica = entr.nextFloat();
+
+        
+
+    }
+}
