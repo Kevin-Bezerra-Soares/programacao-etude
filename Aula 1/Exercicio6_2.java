e62d374fa2181068b105632002a769149660282f
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-03-14 20:32:00 +0000

    main

---
+++ Aula 1/Exercicio6_2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio6_2 {
+    public static void main (String args []){
+        int n;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print(" Informe a quantidade de computadores: ");
+        n = entrada.nextInt();
+
+        if (n%2 == 0){
+            System.out.println(" A quantidade de computadores é par! ");
+        }
+
+        else {
+            System.out.println(" A quantidade de computadores é impar! ");
+        }
+
+    }
+}
