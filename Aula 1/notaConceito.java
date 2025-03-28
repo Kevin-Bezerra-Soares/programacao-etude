dd9297efe99de85a90d6cf5dcb68940cd2be8f10
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-03-28 19:34:17 +0000

    main

---
+++ Aula 1/notaConceito.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class notaConceito {
+    public static void main (String args []){
+        Scanner entrada = new Scanner(System.in);
+        Double nota;
+		char conceito;
+        System.out.print(" Digite a nota do aluno: ");
+		nota = entrada.nextDouble();
+		
+		emitirConceito(nota);
+
+    }
+	
+	public static char emitirConceito(Double nota) {
+		if (nota >= 0.0 && nota <= 4.9){
+			return 'D';
+		}
+		if (nota >= 5.0 && nota <= 6.9){
+			return 'C';
+		}
+		if (nota >= 7.0 && nota <= 8.9){
+			return 'B';
+		}
+		if (nota >= 9.0){
+			return 'A';
+		}
+			
+		return 'F';
+	}
+}
