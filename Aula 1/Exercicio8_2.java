5c66661b78813816b09af494b2a05aa6640ba79f
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-04-03 17:33:34 +0000

    main

---
+++ Aula 1/Exercicio8_2.java
@@ -0,0 +1,19 @@
+
+
+public class Exercicio8_2 {
+    
+    public static void main (String args []){
+
+        int i=1;
+
+        while (i<99){
+            if (i%2 == 0){
+                System.out.printf("|"+i);
+            }
+            System.out.printf("|\n");
+            i++;
+        }
+
+    }
+    
+}
