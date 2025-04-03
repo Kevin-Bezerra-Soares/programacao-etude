1ab4aa6b5dbee37e754fd80e12cda96eebc8feec
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-04-03 20:41:37 +0000

    main

--- Aula 1/Exercicio2_3.java
+++ Aula 1/Exercicio2_3.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class Exercicio2_3 {
 
     public static void main(String[] args) {
+
+        int num,vet1 [], vet2 [];
+        vet1 = new int [5];
+        vet2 = new int [5];
+        Scanner sc = new Scanner(System.in);
+
+        
         
         
 
