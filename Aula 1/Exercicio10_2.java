83251b73178cc342d21514322fcf0a61d6b7f4ee
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-04-03 19:17:23 +0000

    main

---
+++ Aula 1/Exercicio10_2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio10_2 {
+
+    public static void main(String[] args) {
+        int num,cont=0;
+        Scanner entrada = new Scanner(System.in);
+
+        for (int i=0; i<20; i++){
+            System.out.print(" Informe um número inteiro: ");
+            num = entrada.nextInt();
+            if (num < 0){
+                cont++;
+                System.out.println(" Negativo: "+num);
+            }
+        }
+        System.out.println(" Quantidade de negativos: "+cont);
+
+    }
+    
+}
