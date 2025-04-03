fb75746fa96a6f5e141150d043a74036f827f3ff
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-04-03 19:00:31 +0000

    main

---
+++ Aula 1/Exercicio9_2.java
@@ -0,0 +1,15 @@
+
+public class Exercicio9_2 {
+    public static void main(String[] args) {
+        Double tree1=1.50, tree2=1.10;
+        int ano=0;
+
+        while (tree1 >= tree2){
+            tree1 += 0.02;
+            tree2 += 0.03;
+            ano += 1;
+        }
+        System.out.printf("\n\t Levou: "+ano+" anos\n");
+
+    }    
+}
