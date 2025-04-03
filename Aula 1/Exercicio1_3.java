de2c48136a7e27f7d8b1e2a19475bf4b6fd15d58
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-04-03 20:34:06 +0000

    main

--- Aula 1/Exercicio1_3.java
+++ Aula 1/Exercicio1_3.java
@@ -1,11 +1,25 @@
 import java.util.Scanner;
 
 public class Exercicio1_3 {
-
     public static void main(String[] args) {
         
-        
+        int [] num;
+        num = new int [6];
+        int i;
+        Scanner sc = new Scanner(System.in);
 
+        for (i=0; i<=5; i++){
+
+            System.out.println(" Digite este caraio de número: ");
+            num [i] = sc.nextInt();
+
+        }
+
+        for (i=5;i>=0; i--){
+
+            System.out.println("\n|"+num[i]);
+        }
+        
+        
     }
-    
 }
---
+++ Aula 1/Exercicio2_3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exercicio2_3 {
+
+    public static void main(String[] args) {
+        
+        
+
+    }
+    
+}
