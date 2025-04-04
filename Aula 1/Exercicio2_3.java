dc489b93ef664a202fd008b6f8188dc02461258c
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-04-04 20:17:07 +0000

    main

--- Aula 1/Exercicio2_3.java
+++ Aula 1/Exercicio2_3.java
@@ -4,15 +4,26 @@ public class Exercicio2_3 {
 
     public static void main(String[] args) {
 
-        int num,vet1 [], vet2 [];
+        int vet1 [], vet2 [],i;
         vet1 = new int [5];
         vet2 = new int [5];
         Scanner sc = new Scanner(System.in);
 
-        
-        
-        
+        for (i=0; i<5; i++){
 
+            System.out.println(" Digite um número: ");
+            vet1 [i] = sc.nextInt();
+
+        }
+
+        for (i=0; i<5; i++){
+
+            vet2[i] = vet1[i] * 3;
+            System.out.println("|"+vet2[i]);
+
+        }
+        sc.close();
+    
     }
     
 }
