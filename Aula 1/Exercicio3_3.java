bedbbd7888b93ce687938358aa278457f49380fd
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-04-05 13:10:20 +0000

    main

---
+++ Aula 1/Exercicio3_3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio3_3 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vet [], maior,i;
+        vet = new int [5];
+
+        for (i=0; i<5; i++){
+
+            System.out.println("Digite um número: ");
+            vet [i] = sc.nextInt();
+
+        }
+
+
+    }
+}
