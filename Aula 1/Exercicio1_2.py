e89c1ac41b85efbe700ffd68215b1b98c22d5624
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-04-09 19:42:40 +0000

    main

---
+++ Aula 1/Exercicio1_2.py
@@ -0,0 +1,7 @@
+anos  = int(input("\n Digite sua idade: \n"))
+meses = int(input(" Digite os meses: \n"))
+dias  = int(input(" Digite os dias: \n "))
+
+resp = ((anos * 365) + (meses * 30) + dias)
+
+print("\n Sua idade em dias é: ", resp)
