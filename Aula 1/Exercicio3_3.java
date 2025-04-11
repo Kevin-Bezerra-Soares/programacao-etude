239a83d97f309998b2d1ba25e301bd741b2f958b
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-04-11 20:25:56 +0000

    main

--- Aula 1/Exercicio3_3.java
+++ Aula 1/Exercicio3_3.java
@@ -4,15 +4,19 @@ public class Exercicio3_3 {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int vet [], maior,i;
+        int vet [],i;
         vet = new int [5];
+        int maior = vet[0];
 
         for (i=0; i<5; i++){
 
             System.out.println("Digite um número: ");
             vet [i] = sc.nextInt();
+            if (vet[i] > maior){
+                maior = vet[i];
+            }
         }
-        // percorrer todas
+        System.out.println(" O maio número é:" + maior);
 
     }
 }
