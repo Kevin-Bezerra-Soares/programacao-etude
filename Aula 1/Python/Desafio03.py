1c2492983281d73bc2d757f7fb2392f37cb3eb76
Kevin Bezerra Soares <62070604+Kevin-pyth@users.noreply.github.com>
2025-04-12 15:31:30 +0000

    main

--- Aula 1/Python/Desafio03.py
+++ Aula 1/Python/Desafio03.py
@@ -1,7 +1,7 @@
 import clear
 clear
 
-num1 = int(input(" Digite um núemro: "))
+num1 = int(input(" Digite um número: "))
 num2 = int(input(" Digite outro: "))
 
 print(" O resultado é", num1 + num2)
