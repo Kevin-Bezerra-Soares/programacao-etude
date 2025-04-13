6460830d28f4b3d49ed774b9c28b18755041244d
Kevin Bezerra Soares <kevinbezerrasoares42@gmail.com>
2025-04-13 16:06:07 -0300

    exercicio apitulo 3

---
+++ Aula-1/Python/ex3-7.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.11
+import clear
+clear
+
+# Faça um programa que peça dois números inteiros. Imprima a soma desses dois números.
+
+num1 = int(input(" Digite um número: "))
+num2 = int(input(" Digite outro:  "))
+result = num1 + num2
+
+print(" A soma é %d " % result) 
