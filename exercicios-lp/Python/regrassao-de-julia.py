d28cf2ba799e1d171b947052f2a5d675ff152be8
Kevin Bezerra Soares <62070604+Kevin-Bezerra-Soares@users.noreply.github.com>
2025-04-14 21:16:49 +0000

    main

---
+++ exercicios-lp/Python/regrassao-de-julia.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.13
+import clear
+clear
+
+# Criar um programa que mostre uma raiz quadrada pelo método da Regressão de Júlia.
+
+raiz = int(input(" Informe a raiz que você quer procurar: "))
+w = int(input(" Chute um candidato para essa raiz: "))
+candidato = w + 1
+w2 = w * w
+test = 0
+# candidato: w2 + w + candidato
+
+while (test != raiz):
+    
+    test = w2 + w + candidato
+    if (test == raiz):
+        print(" A raiz é:",candidato)
+    w2 = test
+    w = candidato
+    candidato +=1
