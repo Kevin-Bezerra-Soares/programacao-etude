6858a1ea73dbc3ff42bff1193503ac683df1179b
Kevin Bezerra Soares <62070604+Kevin-Bezerra-Soares@users.noreply.github.com>
2025-04-15 18:08:52 +0000

    main

---
+++ exercicios-lp/Exercicio5_3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercicio5_3 {
+    public static void main(String[] args) {
+        clear.limpar();
+        Scanner sc = new Scanner(System.in);
+
+        int vet[], i;
+        vet = new int[50];
+
+        for (i=1; i<50; i++){
+            System.out.println(" Informe um número inteiro: ");
+            vet [i] = sc.nextInt();
+        }
+
+        for (i=1; i<50; i++){
+
+            if (vet[i] > 0){
+                System.out.println(" Os valores positivos são:" + vet[i]);
+            }
+        }
+
+    }
+
+}
