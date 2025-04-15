01ed9498c563b7d68a3d343cc46fb279f1fccf33
Kevin Bezerra Soares <kevinbezerrasoares42@gmail.com>
2025-04-14 23:02:55 -0300

    metodo julia no java

---
+++ exercicios-lp/MetodoDeJulia.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class MetodoDeJulia {
+  
+  public static void main (String[]args) {
+    clear.limpar();
+    int radicando, palpite, candidato, w, teste;
+    Scanner entrada = new Scanner(System.in);
+
+    System.out.println(" Digite o valor do radicando: ");
+    radicando = entrada.nextInt();
+
+    System.out.println(" Digite o palpite: ");
+    palpite = entrada.nextInt();
+    teste = 0;
+
+    candidato = palpite + 1;
+    w = palpite * palpite;
+
+    while (teste != radicando){
+      
+      System.out.println("teste = " + teste);
+
+      teste = w + palpite + candidato;
+      if (teste == radicando){
+        System.out.println(" A raiz é: " + candidato);
+      }
+      if (teste > radicando){
+        System.out.println(" A raiz é irracional, paciência! ");
+        break;
+      }
+      
+      w = teste;
+      palpite = candidato;
+      candidato += 1;
+
+      
+
+    }
+
+    
+  }
+
+}
+
+
