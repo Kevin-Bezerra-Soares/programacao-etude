bca4214911839ead50e6e1ad4987b074ed069e33
Kevin-Bezerra-Soares <kevinbezerrasoares42@gmail.com>
2025-04-17 18:05:05 -0300

    ex413

---
+++ exercicios-lp/Python/ex4-13.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.13
+import clear
+clear
+
+# No programa a seguir, inverta as linhas do if e else, negando a condição. 
+# Adicione as linhas necessárias para funcionar em python.
+
+a = int(input(" Informe o valor de A: "))
+b = int(input(" Informe o valor de B: "))
+
+if (a > b):
+
+    print (f"{b} é maior do que {a}.")
+
+else:
+
+    print (f"{a} é maior do que {b}")
