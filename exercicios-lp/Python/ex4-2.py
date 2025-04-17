8c45518f7f0c41a862bd74c7a05c1b681f993e21
Kevin Bezerra Soares <kevinbezerrasoares42@gmail.com>
2025-04-17 10:51:03 -0300

    excercício 4.2

---
+++ exercicios-lp/Python/ex4-2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.13
+import clear
+clear
+
+# Escreva um programa que pergunte a velocidade do carro de um usuário.
+# Caso ultrapasse 80 km/h, exiba uma mensagem dizendo que o usuário foi multado.
+# Nesse caso, exiba o valor da multa, cobrando R$5 por quilômetro acima de 80 km/h.
+
+velocidade = float(input(" Informe a velocidade: "))
+multa = 0
+km = 0
+if (velocidade > 80):
+
+    km = velocidade - 80
+    multa = km * 5
+
+    print (f" Você ultrapassou o limite de velocidade, foi multado em R${multa}.")
