627cf646c549c9be34c4e9137a9bff48f4469144
Kevin-Bezerra-Soares <kevinbezerrasoares42@gmail.com>
2025-04-17 15:10:11 -0300

    exercicio 4.6

--- exercicios-lp/Python/ex4-3.py
+++ exercicios-lp/Python/ex4-3.py
@@ -4,4 +4,18 @@ clear
 
 # Escreva um programa que leia três números e que imprima o maior e o menor.
 
+a = int(input("\n Informe um número: "))
+b = int(input(" Informe o segundo número: "))
+c = int(input(" Informe o terceiro número: "))
 
+test1 = (a > b and a > c and b > c)
+test2 = (b > a and b > c and a > c)
+test3 = (c > a and c > b and a > b)
+
+if (test2 == True):
+
+    print(f"\n O {b} é maior e o {c} é o menor.")
+
+if (test1 == True):
+
+    print(f"\n O {a} é o maior e o {c} é o menor.")
