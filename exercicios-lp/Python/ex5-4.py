4b127d8c871838810a0f68e3c757d01b2f28b1ba
Kevin Bezerra Soares <kevinbezerrasoares42@gmail.com>
2025-04-22 15:00:44 -0300

    main

---
+++ exercicios-lp/Python/ex5-4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.13
+import clear
+clear
+
+# Modifique o prigrama anterior para imprimir de 1 até o número digitado pelo usuário,
+# mas, desta vez, apenas números ímpares.
+
+fim = int(input("\n Digite o último número a imprimir: "))
+
+x = 1
+
+while (x <= fim):
+
+    print (x)
+    x += 2
