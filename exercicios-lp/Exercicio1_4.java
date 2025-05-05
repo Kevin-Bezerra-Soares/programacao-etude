d841febd0778fabe8989084070d34045e9984b7f
Kevin Bezerra Soares <62070604+Kevin-Bezerra-Soares@users.noreply.github.com>
2025-05-05 19:04:24 +0000

    main

---
+++ exercicios-lp/Exercicio1_4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exercicio1_4 {
+    public static void main(String[] args) {
+        clear.limpar();
+        Scanner entrada = new Scanner(System.in);
+        int matriz[][],i,j;
+        matriz = new int []
+    }
+    
+}
