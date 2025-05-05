1542a6b48b3e292c029b254ad0522f7dc98a77c2
Kevin Bezerra Soares <62070604+Kevin-Bezerra-Soares@users.noreply.github.com>
2025-05-05 19:36:00 +0000

    main

--- exercicios-lp/Exercicio1_4.java
+++ exercicios-lp/Exercicio1_4.java
@@ -4,8 +4,26 @@ public class Exercicio1_4 {
     public static void main(String[] args) {
         clear.limpar();
         Scanner entrada = new Scanner(System.in);
-        int matriz[][],i,j;
+        int matriz[][],i, j, soma=0;
         matriz = new int [4][4];
+
+        for (i=0; i<4; i++){
+            for (j=0; j<4; j++){
+                System.out.print(" Digite os valores da matriz nos índices ["+ (i+1) +"] ["+ (j+1)+"]:");
+                matriz [i][j] = entrada.nextInt();
+                soma += matriz[i][j];
+            }
+        }
+
+        System.out.println("\n\nConteúdo da matriz: ");
+
+        for (i=0; i<4; i++){
+            for (j=0; j<4; j++){
+                System.out.print(matriz[i][j]+"|");
+            }
+            System.out.println(" ");
+        }
+        System.out.println("\n O total, da matriz é: "+ soma);
     }
     
 }
