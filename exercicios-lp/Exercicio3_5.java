5348cba44b02b73f55aebd4fc85f2d4b37dcff30
Kevin Bezerra Soares <62070604+Kevin-Bezerra-Soares@users.noreply.github.com>
2025-05-08 21:18:27 +0000

    Exercicio3_5.java

---
+++ exercicios-lp/Exercicio3_5.java
@@ -0,0 +1,26 @@
+// Escreva um programa que leia uma string e substitua
+// todos os caracteres ‘a’ da string lida por ‘*’. Informe na tela
+// quantos caracteres foram retirados.
+
+import java.util.Scanner;
+
+public class Exercicio3_5 {
+    public static void main(String[] args) {
+        clear.limpar();
+        Scanner input = new Scanner(System.in);
+        char frase, letra = 'a';
+        int i, cont=0;
+        
+        System.out.println("\n Digite alguma coisa: ");
+        StringBuffer str = new StringBuffer(input.nextLine());
+
+        for (i=0; i < str.length(); i++){
+            frase = str.charAt(i);
+            if (letra == frase){
+                str.setCharAt(i, '*');
+                cont++;
+            }
+        }
+        System.out.println(str+"\n Quantidade de caracteres substituídos: "+cont);
+    }
+}
