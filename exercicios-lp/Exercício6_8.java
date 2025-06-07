b62e1a839996207dda2cba770edcb2dc9bcfeb3b
Kevin Bezerra Soares <62070604+Kevin-Bezerra-Soares@users.noreply.github.com>
2025-06-07 12:52:40 +0000

    numero perfeito

--- exercicios-lp/Exercício6_8.java
+++ exercicios-lp/Exercício6_8.java
@@ -4,30 +4,37 @@
 // divisores. (Ex: 6 é perfeito, 6 = 1 + 2 + 3, que são
 // seus divisores).
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Exercício6_8 {
     public static void main(String[] args) {
-        clear.limpar();
+        //clear.limpar();
         Scanner input = new Scanner(System.in);
-        int value,i;
-        boolean perfect=true;
+        long value;
 
         System.out.println("\n Informe um valor para verificar se é perfeito: ");
-        value = input.nextInt();
+        value = input.nextLong();
 
-        for (i=value;i>0;i--){
-            if (value%i != 0){
-                perfect=false;
-            }
+        numPerfeito(value);
+        input.close();
+        
+    }
+
+    public static void numPerfeito(long value){
+        long i,perfect=0;
+
+        for (i=value-1;i>0;i--){
+            if (value%i == 0){
+                perfect += i;
+            } 
         }
-        if (perfect){
+        if (perfect == value){
             System.out.println("\n O valor "+value+" é um número perfeito! ");
         } else {
             System.out.println("\n O valor "+value+" não é um número perfeito! ");
         }
 
-        
     }
     
 }
