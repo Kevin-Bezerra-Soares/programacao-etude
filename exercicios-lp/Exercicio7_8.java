46f2f99a7e59b2c6411651c2afb1d860729279f9
Kevin Bezerra Soares <62070604+Kevin-Bezerra-Soares@users.noreply.github.com>
2025-06-07 13:54:17 +0000

    exercicio 7 ler vetor maior e menor número

---
+++ exercicios-lp/Exercicio7_8.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Exercicio7_8 {
+    public static void main(String[] args) {
+        clear.limpar();
+        Scanner input = new Scanner(System.in);
+        int vet [],i;
+        vet = new int[10];
+
+        for (i=0; i<10;i++){
+            System.out.println("\n Informe os valores do vetor: ");
+            vet [i] = input.nextInt();
+        }
+
+        lerVetor(vet);
+
+    }
+
+    public static void lerVetor(int vet[]){
+        
+        int maior = vet[0],i;
+
+        for (i=0; i<10; i++){
+            if (vet[i] > maior){
+                maior = vet[i];
+            }
+        }
+        System.out.println(" O maior número é: " + maior);
+
+        ///////
+        
+                
+        int menor = vet[0];
+
+        for (i=0; i<10; i++){
+            if (vet[i] < menor){
+                menor = vet[i];
+            }
+        }
+        System.out.println(" O menor número é:" + menor);
+    }
+}
