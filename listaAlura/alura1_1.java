374271054850f81444ce367a09a8cbcafb2e72f9
Kevin Bezerra Soares <62070604+Kevin-Bezerra-Soares@users.noreply.github.com>
2025-06-23 21:04:25 +0000

    Lista alura: média aritmética de 2 n

---
+++ listaAlura/alura1_1.java
@@ -0,0 +1,21 @@
+package listaAlura;
+import java.util.Scanner;
+
+public class alura1_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        clear.limpar();
+
+        System.out.println("\n Informe dois valores inteiros para calcular a média aritmética; ");
+        int val1 = input.nextInt();
+        int val2 = input.nextInt();
+        mediaAritmetica(val1, val2);
+
+        input.close();
+
+    }
+
+    public static void mediaAritmetica(int val1, int val2){
+        System.out.println("A média é: "+((val1+val2)/2));
+    }
+}
---
+++ listaAlura/clear.java
@@ -0,0 +1,15 @@
+/// Classe para limpar a tela do terminal nos códigos
+package listaAlura;
+
+import java.io.IOException;
+
+public class clear {
+    public static void limpar() {
+        try {
+            // Executa o comando clear (via bash, que costuma ser mais confiável)
+            new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
